9931a4cb08bdbc0200f178b608fe3cf16de903b7
B.S.N. Abhiram <abhirambsn@gmail.com>
2023-04-18 20:25:39 +0530

    Docker Done

--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -7,21 +7,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const NGOFunding = await hre.ethers.getContractFactory("NGOFunding");
+  const ngoFunding = await NGOFunding.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  await ngoFunding.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log(`NGO Funding Contract Deployed at ${ngoFunding.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
